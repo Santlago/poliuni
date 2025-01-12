5cd5f960bd33b15cd516e59f7ea98b2dde8e308e
Santlago <breno.santiago.8@gmail.com>
2025-01-12 20:47:53 -0300

    Add Navbar, Footer, and ThemeController components; remove unused SVG files and update layout

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "tailwindcss";
 import daisyui from "daisyui"
+import themes from 'daisyui/src/theming/themes'
 
 export default {
   content: [
@@ -7,13 +8,19 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...themes.light
+        }
       },
-    },
+      {
+        dark: {
+          ...themes.dark
+        }
+      }
+    ],
   },
   plugins: [
     daisyui,
