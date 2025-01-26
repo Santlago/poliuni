f02f8ec5d3e9b2b005d0d6d06a32a1b088f9165a
Santlago <breno.santiago.8@gmail.com>
2025-01-26 12:34:45 -0300

    Add new statistics section and update navbar links for improved navigation

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,5 @@
 import { YouTubeVideo } from '@/components/youtubevideo'
-import { ArrowLeft, ArrowRight } from 'lucide-react'
+import { ArrowLeft, ArrowRight, BookCheck, Globe, GraduationCap } from 'lucide-react'
 import Image from 'next/image'
 
 export default function Home() {
@@ -24,6 +24,44 @@ export default function Home() {
           <div className='flex gap-4'>
             <button className='btn btn-primary lg:hidden'>Comece Agora</button>
             <button className='btn btn-outline rounded-full hidden lg:block' data-theme='dark'>Comece Agora</button>
+          </div>
+        </div>
+      </section>
+      <section className='flex flex-col lg:flex-row gap-8 justify-between'>
+        <div className='flex flex-col items-center lg:w-[30%] gap-4'>
+          <div className='flex items-center gap-3 text-secondary'>
+            <div className='flex flex-col text-right'>
+              <h1 className='text-6xl font-bold'>24</h1>
+              <h2 className='text-3xl font-bold'>Aulas</h2>
+            </div>
+            <BookCheck size={50} />
+          </div>
+          <div className="p-16 w-full bg-base-200 flex-grow">
+            <p className='text-lg font-semibold'>Aulas online interativas e dinâmicas para facilitar o aprendizado.</p>
+          </div>
+        </div>
+        <div className='flex flex-col items-center lg:w-[30%] gap-4'>
+          <div className='flex items-center gap-3 text-warning'>
+            <div className='flex flex-col text-right'>
+              <h1 className='text-6xl font-bold'>10</h1>
+              <h2 className='text-3xl font-bold'>Professores</h2>
+            </div>
+            <GraduationCap size={70} />
+          </div>
+          <div className="p-16 w-full bg-base-200 flex-grow">
+            <p className='text-lg font-semibold'>Professores altamente qualificados e experientes.</p>
+          </div>
+        </div>
+        <div className='flex flex-col items-center lg:w-[30%] gap-4'>
+          <div className='flex items-center gap-3 text-primary'>
+            <div className='flex flex-col text-right'>
+              <h1 className='text-6xl font-bold'>6</h1>
+              <h2 className='text-3xl font-bold'>Idiomas</h2>
+            </div>
+            <Globe size={60} />
+          </div>
+          <div className="p-16 w-full bg-base-200 flex-grow">
+            <p className='text-lg font-semibold'>Idiomas variados para expandir suas habilidades linguísticas.</p>
           </div>
         </div>
       </section>
--- apps/web/src/components/navbar/Navbar.tsx
+++ apps/web/src/components/navbar/Navbar.tsx
@@ -23,20 +23,18 @@ export function Navbar() {
           <ul
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-            <li><a>QUEM SOMOS</a></li>
-            <li><a>O QUE FAZEMOS </a></li>
-            <li><a>O QUE DEFENDEMOS</a></li>
             <li><a>AULAS</a></li>
+            <li><a>QUEM SOMOS</a></li>
+            <li><a>FAÇA PARTE</a></li>
           </ul>
         </div>
         <a className="btn btn-ghost text-xl"><Star />PoliUni</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
-          <li><a>QUEM SOMOS</a></li>
-          <li><a>O QUE FAZEMOS </a></li>
-          <li><a>O QUE DEFENDEMOS</a></li>
           <li><a>AULAS</a></li>
+          <li><a>QUEM SOMOS</a></li>
+          <li><a>FAÇA PARTE</a></li>
         </ul>
       </div>
       <div className="navbar-end flex gap-2">
