9b5eb2fd455ae6b237c6ae1b046a58d39fdf32ed
Santlago <breno.santiago.8@gmail.com>
2025-02-01 20:13:15 -0300

    Refactor video data management by moving video definitions to local storage and simplifying Aulas component

--- apps/web/src/app/aulas/page.tsx
+++ apps/web/src/app/aulas/page.tsx
@@ -1,29 +1,7 @@
 import { YouTubeVideoThumbnail } from '@/components/youtube-video-thumbnail'
-import { Video } from '@/types/video'
+import { videos } from '@/local-storage/videos'
 
 export default function Aulas() {
-  const englishVideos: Video[] = Array(11).fill({
-    title: 'Aula de Inglês',
-    description: 'Aprenda inglês de forma fácil e divertida com nossos professores qualificados.',
-    language: 'inglês',
-    src: 'YHxcxdIpho0'
-  } as Video)
-  const frenchVideos: Video[] = Array(11).fill({
-    title: 'Aula de Francês',
-    description: 'Aprenda francês de forma fácil e divertida com nossos professores qualificados.',
-    language: 'francês',
-    src: 'oS5b-JSM9FY'
-  } as Video)
-  const spanishVideos: Video[] = Array(11).fill({
-    title: 'Aula de Espanhol',
-    description: 'Aprenda espanhol de forma fácil e divertida com nossos professores qualificados.',
-    language: 'espanhol',
-    src: 'QYHv8ArRsF0'
-  } as Video)
-  const videos: Video[] = []
-  videos.push(...englishVideos)
-  videos.push(...frenchVideos)
-  videos.push(...spanishVideos)
 
   const languages = videos.reduce<string[]>((acc, video) => {
     if (!acc.includes(video.language)) {
---
+++ apps/web/src/local-storage/videos.ts
@@ -0,0 +1,25 @@
+import { Video } from '@/types/video'
+
+export const videos: Video[] = []
+
+const englishVideos: Video[] = Array(11).fill({
+  title: 'Aula de Inglês',
+  description: 'Aprenda inglês de forma fácil e divertida com nossos professores qualificados.',
+  language: 'inglês',
+  src: 'YHxcxdIpho0'
+} as Video)
+const frenchVideos: Video[] = Array(11).fill({
+  title: 'Aula de Francês',
+  description: 'Aprenda francês de forma fácil e divertida com nossos professores qualificados.',
+  language: 'francês',
+  src: 'oS5b-JSM9FY'
+} as Video)
+const spanishVideos: Video[] = Array(11).fill({
+  title: 'Aula de Espanhol',
+  description: 'Aprenda espanhol de forma fácil e divertida com nossos professores qualificados.',
+  language: 'espanhol',
+  src: 'QYHv8ArRsF0'
+} as Video)
+videos.push(...englishVideos)
+videos.push(...frenchVideos)
+videos.push(...spanishVideos)
