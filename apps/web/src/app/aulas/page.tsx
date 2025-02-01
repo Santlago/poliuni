b94a62ed34681e94d3de1c05b78ab337e033f45b
Santlago <breno.santiago.8@gmail.com>
2025-02-01 20:25:59 -0300

    Enhance Aulas page layout by adjusting carousel gap and video title styling for better responsiveness

--- apps/web/src/app/aulas/page.tsx
+++ apps/web/src/app/aulas/page.tsx
@@ -17,7 +17,7 @@ export default function Aulas() {
         {languages.map((language, index) => (
           <section key={index} className='my-10 flex flex-col gap-4'>
             <h2 className='text-3xl font-bold'>Aulas de {language}</h2>
-            <div className='carousel flex gap-4 overflow-x-scroll'>
+            <div className='carousel flex gap-4 lg:gap-6 overflow-x-scroll'>
               {videos.map((video, index) => (
                 video.language === language && (
                   <div key={index} className='carousel-item flex flex-col'>
@@ -25,9 +25,9 @@ export default function Aulas() {
                       key={index}
                       title={video.title}
                       src={video.src}
-                      className='h-48 aspect-video'
+                      className='h-48 lg:h-56 aspect-video shadow-lg'
                     />
-                    <h3 className='text-lg font-bold'>{video.title} {index}</h3>
+                    <h3 className='text-lg lg:text-xl font-bold'>{video.title} {index}</h3>
                   </div>
                 )
               ))}
