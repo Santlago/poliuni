5fc41b4a232a390fc4f86a49f410240b8fd742f2
Santlago <breno.santiago.8@gmail.com>
2025-02-02 11:49:19 -0300

    Add support for additional image domain and enhance Aula page layout with new UI elements

--- apps/web/src/app/aula/[id]/page.tsx
+++ apps/web/src/app/aula/[id]/page.tsx
@@ -4,6 +4,8 @@ import { YouTubeVideo } from '@/components/youtube-video'
 import { YouTubeVideoThumbnail } from '@/components/youtube-video-thumbnail'
 import { videos } from '@/local-storage/videos'
 import { Video } from '@/types/video'
+import { BellOff, Bookmark, ChevronDown, Forward, HeartHandshake, ThumbsUp } from 'lucide-react'
+import Image from 'next/image'
 import Link from 'next/link'
 import { useParams } from 'next/navigation'
 
@@ -24,16 +26,26 @@ export default function Aula() {
           </div>
           <div className='flex flex-col gap-2'>
             <h1 className='text-xl font-bold'>{video.title}</h1>
-            <h2 className='text-sm font-semibold'>{video.description}</h2>
-            <div className='flex items-center gap-3'>
-              <div className="btn btn-ghost btn-circle btn-sm avatar overflow-hidden">
-                <div className="w-full rounded-full">
-                  <img
-                    alt="Tailwind CSS Navbar component"
-                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            <h2 className='text-sm font-semibold truncate'>{video.description}</h2>
+            <div className='flex items-center justify-between'>
+              <div className='flex items-center gap-3'>
+                <div className="btn btn-ghost btn-circle btn-sm avatar overflow-hidden">
+                  <div className="w-full rounded-full">
+                    <Image src='https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp' width={200} height={200} alt='avatar' />
+                  </div>
                 </div>
+                <p className='font-semibold'>{video.teacher}</p>
               </div>
-              <p className='font-semibold'>{video.teacher}</p>
+              <div className='btn btn-xs'>
+                <BellOff size={16} className='opacity-70' />
+                <ChevronDown size={16} className='opacity-70' />
+              </div>
+            </div>
+            <div className='flex justify-between'>
+              <div className='btn btn-sm'><ThumbsUp size={16}/>512</div>
+              <div className='btn btn-sm'><Forward size={16}/>Share</div>
+              <div className='btn btn-sm'><Bookmark size={16}/>Save</div>
+              <div className='btn btn-sm'><HeartHandshake size={16}/>Thanks</div>
             </div>
           </div>
         </section>
