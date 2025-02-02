a3fc447dc618e6ff645a99386763059724a02076
Santlago <breno.santiago.8@gmail.com>
2025-02-02 15:29:20 -0300

    Enhance Aula page layout with collapsible description section and improved interactivity

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,8 +1,19 @@
+'use client'
+
 import { YouTubeVideo } from '@/components/youtube-video'
-import { ArrowLeft, ArrowRight, BookCheck, Globe, GraduationCap } from 'lucide-react'
+import clsx from 'clsx'
+import { ArrowLeft, ArrowRight, BookCheck, ChevronDown, ChevronUp, Globe, GraduationCap } from 'lucide-react'
 import Image from 'next/image'
+import { useState } from 'react'
 
 export default function Home() {
+
+  const [isDescriptionOpen, setIsDescriptionOpen] = useState(false)
+
+  const handleClick = () => {
+    setIsDescriptionOpen(!isDescriptionOpen)
+  }
+
   return (
     <div className='p-4'>
       <section className='relative py-32 lg:py-8 mb-10 lg:my-10 -mt-4 -mx-4 text-[#d8d8d8]'>
@@ -68,11 +79,17 @@ export default function Home() {
       </section>
       <section className='card flex bg-base-200 items-center p-4 gap-4 justify-center my-10'>
         <h1 className='text-2xl font-semibold'>Sobre o Projeto</h1>
-        <div className='flex flex-col lg:block gap-4 text-lg text-justify lg:p-8'>
+        <div className={clsx(`flex flex-col lg:h-full overflow-hidden lg:block gap-4 text-lg text-justify lg:p-8 ${isDescriptionOpen ? 'h-auto' : 'h-[400px]'}`)}>
           <YouTubeVideo src='8b3Ea2o9JYA' className='card w-full lg:w-1/3 float-left lg:mx-6 my-2' />
           <p className='indent-8'>Imagine um projeto capaz de abrir portas para o mundo, proporcionando o ensino de idiomas às crianças de uma forma totalmente diferente: divertida, acessível e transformadora. Isso é o PoliUni. Criado com o sonho de tornar o aprendizado de línguas uma experiência envolvente e acessível, o PoliUni se dedica a ensinar idiomas de maneira voluntária, sem custos para as famílias. Nosso objetivo é simples, mas poderoso: oferecer às crianças a chance de sonhar grande, aprender com alegria e se conectar com o mundo.</p>
           <p className='indent-8'>A visão do PoliUni vai além de ensinar idiomas. Queremos transformar a vida de crianças que muitas vezes não são vistas pela sociedade ou que não têm oportunidades iguais. Ao aprenderem um novo idioma, essas crianças expandem seus horizontes e têm a chance de se conectar com o mundo, o que é crucial para um futuro mais inclusivo e educado. Acreditamos que estamos plantando sementes para uma sociedade mais informada e preparada para lidar com a diversidade.</p>
           <p className='indent-8'>Nosso objetivo é ambicioso: queremos levar o PoliUni para diversos países, impactando crianças e jovens de várias partes do mundo. Queremos fazer parte da formação de uma nova geração de jovens que são políglotas, culturalmente conscientes e prontos para construir um mundo melhor. E o mais incrível é que tudo isso acontece de forma gratuita, graças ao trabalho voluntário de pessoas que compartilham o sonho de fazer uma diferença real.</p>
+        </div>
+        <div className='flex flex-col items-center opacity-60 lg:hidden' onClick={handleClick}>
+          <div className='flex flex-col items-center cursor-pointer'>
+            <p className='text-sm'>Leia mais</p>
+            {isDescriptionOpen ? <ChevronUp size={16} /> : <ChevronDown size={16} />}
+          </div>
         </div>
       </section>
       <section className='card flex flex-col items-center p-4 gap-4 justify-center my-10'>
